a15bf45957fa0dd125e266bda18eefb1595cfd75
dvr-games <fayazshaikh0003@gmail.com>
2025-11-18 14:52:34 +0530

    Create analytics.js

---
+++ js/analytics.js
@@ -0,0 +1,8 @@
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+gtag('config', 'G-5XSBWMVXLR', { 
+    page_path: window.location.pathname + window.location.search,
+    page_title: document.title,
+    send_page_view: true
+});
